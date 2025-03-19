8b7af664b6b3eb3cc424bd9324dc147c7f2a17a0
1811htsang <macsin233@gmail.com>
2025-03-19 11:36:22 +0700

    update: done backend and switching frame between authentication and main function frame

---
+++ FinancialManagement/src/main/java/shanghuang/financialmanagement/FinancialManagement.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package shanghuang.financialmanagement;
+
+/**
+ *
+ * @author shanghuang
+ */
+public class FinancialManagement {
+    public static void main(String[] args) {
+        AuthenticationFrame.main(args);
+    }
+}
